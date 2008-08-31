70afecd2667861d115bb965332844f9642954bce
schillacia <schillacia>
2008-08-31 06:49:52 +0000

    *** empty log message ***

--- wide/main.cpp
+++ wide/main.cpp
@@ -1225,7 +1225,9 @@ void MyFrame::OnCompile (wxCommandEvent &event) {
         zcode = mainFile;        
     }
     zcode.Replace(_T(".inf"), zcodeversion, true);
-    wxString comando =  informCompiler+_T(" ")+zcodeswitch+_T(" ")+libraryPath+_T(" \"")+nome+_T("\" ")+_T(" \"")+zcode+_T("\" ");
+    //wxString comando =  informCompiler+_T(" ")+zcodeswitch+_T(" ")+libraryPath+_T(" \"")+nome+_T("\" ")+_T(" \"")+zcode+_T("\" ");
+    wxString comando =  informCompiler+_T(" ")+zcodeswitch+_T(" ")+libraryPath+_T(" \"")+nome+_T("\" ")+_T(" \"")+zcode+_T("\"");
+
     
     //if ( wxExecute(_T("./inform.exe twocol.inf"), output) != 0 )
     if ( wxExecute(comando, output) != 0 )    
@@ -1707,18 +1709,15 @@ void MyFrame::OnLicense(wxCommandEvent& WXUNUSED(event))
     info.SetDescription(wxString::FromAscii(
 "\nGNU GPL LICENSE\n"
 "\n"
-"  This program is free software; you can redistribute it and/or modify\n"
-"  it under the terms of the GNU General Public License as published by\n"
-"  the Free Software Foundation; either version 3 of the License, or\n"
-"  (at your option) any later version.\n"
-"\n"
-"  This program is distributed in the hope that it will be useful,\n"
-"  but WITHOUT ANY WARRANTY; without even the implied warranty of\n"
-"  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the\n"
-"  GNU General Public License for more details.\n"
-"\n"
-"  You should have received a copy of the GNU General Public License\n"
-"  along with this program.  If not, see <http://www.gnu.org/licenses/>.\n"
+"This program is free software; you can redistribute it and/or modify it"
+"under the terms of the GNU General Public License as published by the  "
+"Free Software Foundation; either version 3 of the License, or (at your "
+"option) any later version. This program is distributed in the hope that"
+"it will be useful,  but WITHOUT ANY WARRANTY; without even the implied "
+"warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See   "
+"the GNU General Public License for more details. You should have       "
+"received a copy of the GNU General Public License along with this      "
+"program. If not, see <http://www.gnu.org/licenses/>."
     ));
     wxAboutBox(info);
 }
